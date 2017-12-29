6b23770476a8b2406edcb1eecdf00ffa496f0a1d
mary2002 <m.mirlohi2002@gmail.com>
2017-12-29 17:04:41 +0330

    one number is received from the user and It's printed

--- 1-helloworld.c
+++ 1-helloworld.c
@@ -5,5 +5,8 @@ int main(int argc, char const *argv[])
 	int b;
 	int a=110;
 	printf("hello world%d\n",a);
+	printf("Enter a number:");
+	scanf("%d", &b);
+	printf("Your number:%d",b);
 	return 0;
 }
