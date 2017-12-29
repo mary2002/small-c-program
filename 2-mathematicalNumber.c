44872c0c7acbd22894dc9ce6e8beed2e9aca0bda
mary2002 <m.mirlohi2002@gmail.com>
2017-12-29 17:23:48 +0330

    The sum function was added

--- 2-mathematicalNumber.c
+++ 2-mathematicalNumber.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-
+float sum(float,float);
 int main(int argc, char const *argv[])
 {
 	float x,y;
 	printf("Enter two numbers(the second numbers can't be zero):");
 	scanf("%f%f",&x,&y);
 	printf("first number:%f and second number:%f\n",x,y);
+	printf("sum:%f\n",sum(x,y));
 	return 0;
+}
+float sum(float a, float b){
+	return a+b;
 }
