cd37c6649963646be20119bf4dc8d3ce55339159
mary2002 <m.mirlohi2002@gmail.com>
2017-12-29 17:20:20 +0330

    In this project 2 numbers are received from the user and printed

---
+++ 2-mathematicalNumber.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	float x,y;
+	printf("Enter two numbers(the second numbers can't be zero):");
+	scanf("%f%f",&x,&y);
+	printf("first number:%f and second number:%f\n",x,y);
+	return 0;
+}
