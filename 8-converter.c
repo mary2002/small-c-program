9425ead9d54ca5c649a2d1f0d6b6bc4ab6addeb8
mary2002 <m.mirlohi2002@gmail.com>
2017-12-30 02:34:50 +0330

    The code that converts your Fahrenheit to Celsius was added

--- 8-converter.c
+++ 8-converter.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 float poundTOkg(float);
-
+float FtoC(float);
 int main(int argc, char const *argv[])
 {
-	float x;
+	float x,f;
 	printf("Enter a pound to convert to kilogram:");
 	scanf("%f",&x);
 	printf("kilogram:%f\n",poundTOkg(x));
+	printf("Enter a Fahrenheit degree to convert to Celsius degree:");
+	scanf("%f",&f);
+	printf("Celsius:%f\n",FtoC(f));
 	return 0;
 }
 float poundTOkg(float a){
 	return a*0.453592;
+}
+float FtoC(float a){
+	return (a-32)*1.8;
 }
