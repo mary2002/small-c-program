103dd2903fbda02766cc635b168eb517af43d45e
mary2002 <m.mirlohi2002@gmail.com>
2017-12-29 20:29:13 +0330

    In this case Square width is taken and the environment and area are calculated

--- 3-areaANDenvironment.c
+++ 3-areaANDenvironment.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 float areaRec(float,float);
 float envireRec(float,float);
+float areaSqr(float);
+float envirSqr(float);
 int main(int argc, char const *argv[])
 {
-	float x,y;
+	float x,y,z;
 	printf("Enter the width of the rectangel:");
 	scanf("%f",&x);
 	printf("Enter the length of the rectangle:");
 	scanf("%f",&y);
 	printf("rectangle area:%f\n",areaRec(x,y));
 	printf("rectangle environment:%f\n",envireRec(x,y));
+	printf("Enter the width of the Square:");
+	scanf("%f",&z);
+	printf("rectangle area:%f\n",areaSqr(z));
+	printf("rectangle environment:%f\n",envirSqr(z));
 	return 0;
 }
 float areaRec(float a, float b){
@@ -17,4 +23,10 @@ float areaRec(float a, float b){
 }
 float envireRec(float a, float b){
 	return (a+b)*2;
+}
+float areaSqr(float a){
+	return a*a;
+}
+float envirSqr(float a){
+	return a*4;
 }
