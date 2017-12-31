81cabbde0b73c852a34d4cbff4daf31dabdd7bda
mary2002 <m.mirlohi2002@gmail.com>
2017-12-31 22:29:35 +0330

    This program gets two numbers and checks numbers between them if the number equals of the cube sum of digits is an armstrong number so  prints it

--- 21.printsArmstrongBetween2numbers.c
+++ 21.printsArmstrongBetween2numbers.c
@@ -2,7 +2,14 @@
 int cubeSumOFdigits(int);
 int main(int argc, char const *argv[])
 {
-	
+	int num1,num2;
+	printf("Enter tow numbers:");
+	scanf("%d%d",&num1,&num2);
+	for (int i = num1+1; i < num2; ++i)
+	{
+		if (i == cubeSumOFdigits(i))
+			printf("%d ",i);
+	}
 
 	return 0;
 }
