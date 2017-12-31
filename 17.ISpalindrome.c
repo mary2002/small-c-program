b992966bef090f80579e2c5c7b94c8c75cd18aee
mary2002 <m.mirlohi2002@gmail.com>
2017-12-31 21:01:16 +0330

    This code compares the number and inverse of it, and if they are equal,It writes the number is palindrome

--- 17.ISpalindrome.c
+++ 17.ISpalindrome.c
@@ -2,7 +2,13 @@
 int revers(int);
 int main(int argc, char const *argv[])
 {
-	
+	int num;
+	printf("Enter a number:");
+	scanf("%d",&num);
+	if (num == revers(num))
+        printf("%d is a palindrome. \n", num);
+    else
+        printf("%d is not a palindrome. \n", num);
 	return 0;
 }
 int revers(int num){
