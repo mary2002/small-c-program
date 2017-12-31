5a2948cf95572897df368f5433b9845eb9022d6a
mary2002 <m.mirlohi2002@gmail.com>
2017-12-31 19:43:55 +0330

    This code is taken a number from user and prints the first n sentences Fibonacci series

--- 11.Fibonacci.c
+++ 11.Fibonacci.c
@@ -3,6 +3,14 @@ int Fibonacci(int n);
 
 int main(int argc, char const *argv[])
 {
+	int num;
+	printf("Enter a number:");
+	scanf("%d",&num);
+	for (int i = 0; i < num; ++i)
+	{
+	printf("%d,",Fibonacci(i));
+	}
+	printf("...\n");
 	
 	return 0;
 }
