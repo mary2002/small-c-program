9127a0d0f4d05720a570a56c73bcab5a2652cd9d
mary2002 <m.mirlohi2002@gmail.com>
2017-12-31 20:00:25 +0330

    The lcm function with the help of the function gcd calculates lcm input tow numbers

--- 12.lcm.c
+++ 12.lcm.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 int gcd(int,int);
+int lcm(int,int);
 int main(int argc, char const *argv[])
 {
+	int a,b;
+	printf("Enter two numbers:");
+	scanf("%d%d",&a,&b);
+	printf("lcm=%d\n",lcm(a,b));
 	return 0;
 }
 int gcd(int a,int b){
 	if(b==0)
 		return a;
 	return gcd(b,a%b) ;
+}
+int lcm(int a,int b){
+	return (a*b)/gcd(a,b);
 }
