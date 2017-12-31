ed2faace45e959f8220db4856bbbcbe19a078ff5
mary2002 <m.mirlohi2002@gmail.com>
2017-12-31 20:56:00 +0330

    This function gets a number and reverses it

---
+++ 17.ISpalindrome.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int revers(int);
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
+int revers(int num){
+	int rev=0;
+	while(num!=0)
+	{
+		rev=(10*rev)+(num%10);
+		num/=10;
+	}
+	return rev;
+}
