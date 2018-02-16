d152fbaff9f010200ccd6fe57c8fcfefa4725a3a
mary2002 <m.mirlohi2002@gmail.com>
2018-02-16 16:09:40 +0330

    fixing multiply bug

--- Argument.c
+++ Argument.c
@@ -7,16 +7,14 @@ int main(int argc, char const *argv[])
 		printf("There is'nt any arguments\n" );
 		return 1;
 	}
-
 if(strcmp(argv[2], "+") == 0)
 	printf("%d\n", atoi(argv[1])+atoi(argv[3]));
-if(strcmp(argv[2], "-") == 0)
+else if(strcmp(argv[2], "-") == 0)
 	printf("%d\n", atoi(argv[1])-atoi(argv[3]));
-if(strcmp(argv[2], "*") == 0)
-	printf("%d\n", (atoi(argv[1]))*(atoi(argv[3])));
-if(strcmp(argv[2], "/") == 0)
+else if(strcmp(argv[2], "/") == 0)
 	printf("%d\n", atoi(argv[1])/atoi(argv[3]));
-
-
+else{
+	printf("%d\n", atoi(argv[1]) * atoi(argv[argc-1]));
+}
 	return 0;
 }
